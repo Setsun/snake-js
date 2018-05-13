72356d9b6911fbe35107e2bd036d876a9dae0cee
Setsun <seanjeluo@gmail.com>
2018-05-13 16:06:35 -0400

    render the grid

--- src/index.js
+++ src/index.js
@@ -28,4 +28,27 @@ const initializeInterval = () => {
   }, 1000);
 };
 
+const render = (grid, snake) => {
+  const rootEl = document.getElementById('root');
+  let gridEl = document.createElement('div');
+  gridEl.classList.add('grid');
+
+  for (let row of grid) {
+    let rowEl = document.createElement('div');
+    rowEl.classList.add('row');
+
+    for (let position of row) {
+      let cellEl = document.createElement('div');
+      cellEl.classList.add('cell');
+      rowEl.appendChild(cellEl);
+    }
+
+    gridEl.appendChild(rowEl);
+  }
+
+  rootEl && rootEl.appendChild(gridEl);
+};
+
 console.log('hello world');
+
+render(generateGrid(10));
