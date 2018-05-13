ced06fcebd63a141008da0b8f58c501203881e64
Setsun <seanjeluo@gmail.com>
2018-05-13 15:45:59 -0400

    basic functions for snake

--- src/index.js
+++ src/index.js
@@ -1,1 +1,38 @@
+const generateGrid = () => {
+
+};
+
+const generateSnake = () => {
+
+};
+
+const generateApple = () => {
+
+};
+
+const initializeKeyEvents = () => {
+  document.addEventListener('keydown', (event) => {
+    switch(event.key) {
+      case 37: {
+
+      }
+      case 38: {
+
+      }
+      case 39: {
+
+      }
+      case 40: {
+
+      }
+    }
+  });
+};
+
+const initializeInterval = () => {
+  setInterval(() => {
+
+  }, 1000);
+};
+
 console.log('hello world');
